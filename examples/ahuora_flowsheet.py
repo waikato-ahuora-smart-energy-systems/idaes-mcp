761fde3a1fec91436e61137f5a104ff377cf7b65
bd65 <bd65@students.waikato.ac.nz>
2026-02-12 09:59:05 +1300

    loading ahuora flowsheets

---
+++ examples/ahuora_flowsheet.py
@@ -0,0 +1,36 @@
+import os
+import json
+import io
+from ahuora_builder.flowsheet_manager import FlowsheetManager
+from ahuora_builder_types.flowsheet_schema import FlowsheetSchema
+from idaes.core.util.model_diagnostics import DiagnosticsToolbox
+
+
+INPUT_FILE = "json/model.json"
+
+# Get current location (so that we can retrieve pump.json)
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+with open(os.path.join(__location__, INPUT_FILE), 'r') as file:
+
+    data = json.load(file)
+
+    flowsheet_schema = FlowsheetSchema.model_validate(data)
+    flowsheet = FlowsheetManager(flowsheet_schema)
+    flowsheet.load()
+    flowsheet.initialise()
+    assert flowsheet.degrees_of_freedom() == 0, "Degrees of freedom is not 0: " + str(flowsheet.degrees_of_freedom())
+    flowsheet.report_statistics()
+    try:
+        flowsheet.solve()
+    except Exception as e:
+        print(e)
+    flowsheet.diagnose_problems()
+    
+
+    m = flowsheet.model
+
+    dt = DiagnosticsToolbox(m)
+
+    dt.display_constraints_with_large_residuals()
