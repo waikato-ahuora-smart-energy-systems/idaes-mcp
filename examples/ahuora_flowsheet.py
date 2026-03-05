fc1c045eea6ff9bb48ead31dca5ea0bc651d84c4
bd65 <bd65@students.waikato.ac.nz>
2026-03-06 12:49:38 +1300

    separate graph and prepare to debug/scale evaporator

--- examples/ahuora_flowsheet.py
+++ examples/ahuora_flowsheet.py
@@ -21,8 +21,9 @@ from idaes.core.util.scaling import get_scaling_factor
 from idaes.core.scaling import AutoScaler
 from idaes.core.scaling.util import get_jacobian
 from idaes.core.util.model_diagnostics import SVDToolbox
+from generate_graph import generate_graph
 
-INPUT_FILE = "json/evaporator_dsi.json"
+INPUT_FILE = "json/model.json"
 
 # Get current location (so that we can retrieve pump.json)
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -59,76 +60,20 @@ with open(os.path.join(__location__, INPUT_FILE), 'r') as file:
     # dt.display_variables_with_extreme_jacobians()
 
 
-    try:
-        flowsheet.solve()
-    except Exception as e:
-        print(e)
+    # try:
+    #     flowsheet.solve()
+    # except Exception as e:
+    #     print(e)
     # flowsheet.diagnose_problems()
 
-    flowsheet.properties_map.items()
 
-    svd_toolbox = dt.prepare_svd_toolbox()
-    svd_toolbox.display_underdetermined_variables_and_constraints()
+    # svd_toolbox = dt.prepare_svd_toolbox()
+    # svd_toolbox.display_underdetermined_variables_and_constraints()
 
-    dt.display_extreme_jacobian_entries()
+    #dt.display_extreme_jacobian_entries()
 
-    jacobian, nlp = get_jacobian(m)
-    # dictionay [ID of of variable using python id(ConstraintData object), index of constraint in nlp.clist]
-    cmap : dict[int,int] = {
-        id(constraint): index for index,constraint in enumerate(nlp.clist)
-    }
-    # dictionay [ID of of variable using python id(ScalarVar), index of variable in nlp.vlist]
-    vmap : dict[int, int] = {
-        id(variable): index for index,variable in enumerate(nlp.vlist)
-    }
+    # generate_graph(m, graph_path="graph.html")
 
-
-    
-
-    
-
-
-    graph, number_component_map, constraint_variable_map = generate_model_graph(m,"bipartite")
-
-    net = Network(notebook=True, cdn_resources="remote")
-
-    # this is a bipartite graph, so we can color the two sets of nodes differently
-    for node in graph.nodes():
-        component = number_component_map[node]
-        current_value :float = pyo.value(component)
-        scaling_factor = get_scaling_factor(component)
-        if scaling_factor is None:
-            scaling_factor = 1
-        if isinstance(component, ConstraintData):
-            target_value :float = pyo.value(component.upper)
-            infeasibility = abs(current_value - target_value)
-            size = log((infeasibility*scaling_factor*10**14)+1)+1
-            net.add_node(node, color='red', title=str(component) + f" ({'{:.2e}'.format(infeasibility)}) (SF: {'{:.2e}'.format(scaling_factor)})", size=size)
-        else:
-            if not component.is_fixed():
-                net.add_node(node, color=( 'lightblue' if component.is_fixed() else 'blue'),title=str(component) + f" ({'{:.2e}'.format(current_value)}) (SF: {'{:.2e}'.format(scaling_factor)})", size=10)
-    for edge in graph.edges():
-        # only add edge if both nodes are in the graph (since pyvis will throw an error otherwise)
-        # this allows us to only visualise part of the graph if we want to.
-        if edge[0] in net.node_ids and edge[1] in net.node_ids:
-            # find the variable and constraint associated with this edge
-            edge0 = number_component_map[edge[0]]
-            edge1 = number_component_map[edge[1]]
-            if isinstance(edge0, ConstraintData):
-                # as this is a bipartite graph, we know that the other edge must be a variable
-                variable = edge1
-                constraint = edge0
-            else:
-                variable = edge0
-                constraint = edge1
-            # get the indexes of the variable and constraint in the jacobian, and get the jacobian value
-            jacobian_value = jacobian[cmap[id(constraint)], vmap[id(variable)]]
-            # weight the edge by the log of the jacobian value
-            net.add_edge(edge[0], edge[1], value=(abs(jacobian_value)+0.000001), title='{:.2e}'.format(jacobian_value),
-                         color=("#333333" if jacobian_value != 0 else "#440099"))
-
-
-    net.show("graph.html")
     # prop = flowsheet.properties_map.get(463458)
 
 
--- examples/generate_graph.py
+++ examples/generate_graph.py
@@ -3,6 +3,7 @@ from pyvis.network import Network
 from pyomo.core.base.constraint import ConstraintData
 import pyomo.environ as pyo
 from math import log
+from idaes.core.scaling.util import get_jacobian
 from idaes.core.util.scaling import get_scaling_factor
 
 def generate_graph(block : pyo.Block, show_fixed: bool=False, graph_path="graph.html"):
@@ -10,8 +11,18 @@ def generate_graph(block : pyo.Block, show_fixed: bool=False, graph_path="graph.
     graph, number_component_map, constraint_variable_map = generate_model_graph(block,"bipartite")
 
     net = Network(notebook=True, cdn_resources="remote")
+    jacobian, nlp = get_jacobian(block)
 
-    # this is a bipartite graph, so we can color the two sets of nodes differently
+    # dictionay [ID of of variable using python id(ConstraintData object), index of constraint in nlp.clist]
+    cmap : dict[int,int] = {
+        id(constraint): index for index,constraint in enumerate(nlp.clist)
+    }
+    # dictionay [ID of of variable using python id(ScalarVar), index of variable in nlp.vlist]
+    vmap : dict[int, int] = {
+        id(variable): index for index,variable in enumerate(nlp.vlist)
+    }
+
+
     for node in graph.nodes():
         component = number_component_map[node]
         current_value :float = pyo.value(component)
@@ -21,15 +32,29 @@ def generate_graph(block : pyo.Block, show_fixed: bool=False, graph_path="graph.
         if isinstance(component, ConstraintData):
             target_value :float = pyo.value(component.upper)
             infeasibility = abs(current_value - target_value)
-            size = log((infeasibility*scaling_factor*10**15)+1)+1
+            size = log((infeasibility*scaling_factor*10**14)+1)+1
             net.add_node(node, color='red', title=str(component) + f" ({'{:.2e}'.format(infeasibility)}) (SF: {'{:.2e}'.format(scaling_factor)})", size=size)
         else:
-            if not component.is_fixed() or show_fixed:
+            if not component.is_fixed():
                 net.add_node(node, color=( 'lightblue' if component.is_fixed() else 'blue'),title=str(component) + f" ({'{:.2e}'.format(current_value)}) (SF: {'{:.2e}'.format(scaling_factor)})", size=10)
     for edge in graph.edges():
         # only add edge if both nodes are in the graph (since pyvis will throw an error otherwise)
         # this allows us to only visualise part of the graph if we want to.
         if edge[0] in net.node_ids and edge[1] in net.node_ids:
-            net.add_edge(edge[0], edge[1],color="gray")
+            # find the variable and constraint associated with this edge
+            edge0 = number_component_map[edge[0]]
+            edge1 = number_component_map[edge[1]]
+            if isinstance(edge0, ConstraintData):
+                # as this is a bipartite graph, we know that the other edge must be a variable
+                variable = edge1
+                constraint = edge0
+            else:
+                variable = edge0
+                constraint = edge1
+            # get the indexes of the variable and constraint in the jacobian, and get the jacobian value
+            jacobian_value = jacobian[cmap[id(constraint)], vmap[id(variable)]]
+            # weight the edge by the log of the jacobian value
+            net.add_edge(edge[0], edge[1], value=(abs(jacobian_value)+0.000001), title='{:.2e}'.format(jacobian_value),
+                         color=("#333333" if jacobian_value != 0 else "#440099"))
 
     net.show(graph_path)
